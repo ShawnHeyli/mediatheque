eae6bc079431c6c2d9e0daec2b59ebb995599643
L0kaar <nicoshan418@yahoo.fr>
2023-05-13 01:33:00 +0200

    page.jsx added to test features
    page.jsx added to test features + modif movieList

--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -43,14 +43,14 @@ export default function MovieList(props) {
 
     return <div className="movieList">
       <h1>
-          {props.title} {index}
+          {props.title} {cardSize}
       </h1>
-      <div className="cardList" style={{ width: cardSize*250 + 130}}>
+      <div className="cardList" style={{width: cardSize * 250 + 130}}>
         <div className="button" onClick={() => {updateIndex(index - 1)}}>
           <svg viewBox="0 0 320 512"> <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
         </div>
 
-        <div className="cardBox">
+        <div className="cardBox" style={{width: cardSize * 250 + 10}}>
           <div className="cardElements" style={{ transform: `translate(-${index * 250}px)`}}>
             {movies?.map((movie)=>
               <div className="cardElement" key={movie.id} name={movie.title} id={movie.id}><Card id={movie.id}/></div>)}
