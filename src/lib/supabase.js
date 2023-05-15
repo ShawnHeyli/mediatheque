07212bc3262a7c8eb93b054ca740f3174e7efb4a
Hilan Meyran <hilanmeyran@protonmail.com>
2023-05-15 13:56:41 +0200

    fix supabase client linting error

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,4 @@
-/* eslint-disable no-undef */
 import { createClient } from "@supabase/supabase-js";
-
-// TODO: Fix
-// EsLint does not recognize process.env
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
