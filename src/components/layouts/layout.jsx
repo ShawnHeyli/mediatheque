879aa58e9b691f3b92bd3d226af28ffe7c9e2c6e
Louis <louis.barbier.crepy@gmail.com>
2023-05-15 13:30:22 +0200

    quick fix page and background

--- src/components/layouts/layout.jsx
+++ src/components/layouts/layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from '@/components/navbar/navbar';
 import Footer from '@/components/footer/footer';
+import "./layout.scss";
 
 export default function Layout({children}) {
   return (
     <>
       <NavBar/>
-      <main>
+      <main className="container">
         {children}
       </main>
       <Footer/>
