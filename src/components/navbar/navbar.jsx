5719823a87fa669ee69f7349ce5565e6bc6a27cb
Louis <louis.barbier.crepy@gmail.com>
2023-05-16 15:58:24 +0200

    add front-login v0

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,10 +5,9 @@ export default function NavBar (){
   return (
   <header className="navBar">
     <ul>
-      <li><a href="/accueil">CINERATER</a></li>
+      <li><a href="/">CINERATER</a></li>
       <li className="user"><a className="active right" href="#inscription">Inscription</a></li>
-      <li className="user"><a href="#se connecter">Se connecter</a></li>
-      
+      <li className="user"><a href="/login">Se connecter</a></li>
     </ul>
   </header>
   )
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,6 +4,7 @@ import "./index.scss";
 import { Link, Grid, TextField, Button, Avatar } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Layout from "@/components/layouts/layout";
 
 export default function SignInSide() {
   // eslint-disable-next-line no-unused-vars
@@ -24,53 +25,87 @@ export default function SignInSide() {
     typography: {
       fontFamily: ["Rubik", "system-ui", "sans-serif"].join(","),
     },
+    palette: {
+      primary: {
+        main: '#7f5af0',
+      },
+      neutral: {
+        main: '#2cb67d',
+        contrastText: '#fffffe',
+      },
+    },
   });
+  
 
   return (
+    <Layout>
     <ThemeProvider theme={theme}>
       <div className="login">
         <div className="login_form">
           <Avatar sx={{ m: 1, bgcolor: "#7f5af0" }}>
             <AccountCircleIcon />
           </Avatar>
-          <TextField
+          <TextField className="input"
             margin="normal"
             required
             fullWidth
+            color="neutral"
             id="email"
             label="Email"
             name="email"
             autoComplete="email"
             autoFocus
+            inputProps={{
+              sx: {
+                color: '#fffffe',
+              },
+            }}
+            InputLabelProps={{
+              sx: {
+                color: '#7f5af0',
+              },
+            }}
           />
-          <TextField
+          <TextField className="input"
             margin="normal"
             required
             fullWidth
             name="password"
             label="Password"
+            color="neutral"
             type="password"
             id="password"
             autoComplete="current-password"
+            inputProps={{
+              sx: {
+                color: '#fffffe',
+              },
+            }}
+            InputLabelProps={{
+              sx: {
+                color: '#7f5af0',
+              },
+            }}
           />
-          <Button
+          <Button className="se_connecter"
             type="submit"
             fullWidth
             variant="contained"
             sx={{ mt: 3, mb: 2, bgcolor: "#7f5af0" }}
           >
-            Sign In
+            se connecter
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#">Forgot password?</Link>
+              <Link className="link" href="#">Mot de passe oublié ?</Link>
             </Grid>
             <Grid item>
-              <Link href="#">{"Don't have an account? Sign Up"}</Link>
+              <Link className="link" href="#inscription">{"Vous n'avez pas de compte ? Inscrivez vous"}</Link>
             </Grid>
           </Grid>
         </div>
       </div>
     </ThemeProvider>
+    </Layout>
   );
 }
