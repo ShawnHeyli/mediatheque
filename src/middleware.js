5956449de1f4e7c7b9a7f0a0144c9038b5a58394
Hilan Meyran <hilanmeyran@protonmail.com>
2023-05-18 12:04:39 +0200

    fix middleware CSP policy

--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server';
 
 export const config = {
-  matcher: '/api/:path*',
+  matcher: [ "/api/:path*", "/:path*" ],
 };
 
 export function middleware(request) {
   // Set some security headers to help protect against common web vulnerabilities
   const response = NextResponse.next();
   response.headers.set('X-Frame-Options', 'SAMEORIGIN'); // Prevent Clickjacking attacks
-  response.headers.set('X-XSS-Protection', '1; mode=block'); // Enable XSS filtering
+  response.headers.set('X-XSS-Protection', '1; mode=block') ; // Enable XSS filtering
   response.headers.set('X-Content-Type-Options', 'nosniff'); // Prevent MIME type sniffing
   response.headers.set('Referrer-Policy', 'no-referrer'); // Hide the Referer header when leaving your site
-  response.headers.set('Content-Security-Policy', "default-src 'self'"); // Limit resources to only come from your site
+  response.headers.set('Content-Security-Policy', "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'unsafe-eval' 'self'"); // Limit resources to only come from your site
   
   // Check if the request is a POST request
   if (request.method === 'POST') {
