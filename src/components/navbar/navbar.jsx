df752bed9e92ab85a8ab3e81c8c0c8a4fc37465b
L0kaar <nicoshan418@yahoo.fr>
2023-05-25 21:44:40 +0200

    logos

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import './navBar.scss';
 import SearchBar from "@/components/searchBar/searchBar";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavBar({ searchBar }){
   const hasSearch = (searchBar != "hidden");
 
   return (
     <header className="navBar">
-      <div className="logo"><Link href="/">CINERATER</Link></div>
+      <div className="logo">
+        <Link href="/">
+          <Image
+          src={"/images/svg/logo.svg"}
+          alt="Logo CineRater"
+          height="40" 
+          width="150"
+          />
+        </Link>
+      </div>
       {hasSearch?(
         <SearchBar/>
       ):(
