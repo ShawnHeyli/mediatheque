27477dbc7f0200b5cd9712a2972858da4534613f
Hilan Meyran <hilanmeyran@protonmail.com>
2023-06-05 14:47:52 +0200

    Session-aware navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,41 +3,48 @@ import './navBar.scss';
 import SearchBar from "@/components/searchBar/searchBar";
 import Link from "next/link";
 import Image from "next/image";
+import { useUser } from "@supabase/auth-helpers-react";
 
-export default function NavBar({ searchBar }){
-  const hasSearch = (searchBar != "hidden");
+export default function NavBar({ searchBar }) {
+  const hasSearch = searchBar != "hidden";
+
+  const user = useUser();
+
+  console.log(user);
 
   return (
     <header className="navBar">
       <div className="logo">
         <Link href="/">
           <Image
-          src={"/images/svg/logo.svg"}
-          alt="Logo CineRater"
-          height="40" 
-          width="150"
+            src={"/images/svg/logo.svg"}
+            alt="Logo CineRater"
+            height="40"
+            width="150"
           />
         </Link>
       </div>
-      {hasSearch?(
-        <SearchBar/>
-      ):(
-      <></>
-      )}
+      {hasSearch ? <SearchBar /> : <></>}
       <div className="user">
-        <Link className="account" href="/account">
-          <Image
-          src={"/images/placeholders/default_user_avatar_highlight.png"}
-          alt="profile picture"
-          height="20"
-          width="20"
-          />
-        </Link>
-        <Link className="active" href="/signup">Sign Up</Link>
-        <Link href="/login">Login</Link>
+        {user ? (
+          <Link className="account" href="/account">
+            <Image
+              src={"/images/placeholders/default_user_avatar_highlight.png"}
+              alt="profile picture"
+              height="20"
+              width="20"
+            />
+          </Link>
+        ) : (
+          <>
+            <Link className="active" href="/signup">
+              Sign Up
+            </Link>
+            <Link href="/login">Login</Link>
+          </>
+        )}
       </div>
-      
     </header>
-  )
+  );
 }
 
